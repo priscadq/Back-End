61a17f4d6f9335dbcb3d2016b8e7951c33a2582e
priscadq <priscadq@gmail.com>
2018-04-07 12:09:52 -0300

    send!
    update

--- Clase1/servidor2740.js
+++ Clase1/servidor2740.js
@@ -36,3 +36,32 @@ var server = http.createServer(function(request, response) {
 });
 
 
+/* 
+-------------------------------------
+otro metodo de cargar un servidor, cargando el html
+--------------------------------------
+var http = require("http");
+var fs = require("fs")
+
+var server = http.createServer(function(request, response) {
+ response.writeHead(200, {"Content-Type": "text/html"});
+ fs.readFile(__dirname + '/index.html', function(err, data){
+   // Manejo de errores debería estar acá.
+   response.end(data);
+ });  
+});
+
+server.listen(80);
+console.log("Server is listening");
+--------------------------------------
+html
+---------------------------------------
+<!DOCTYPE html>
+<html>
+<head>
+   <title>Clase Backend</title>
+</head>
+<body>
+Buenas noches clase!!
+</body>
+</html>*/
