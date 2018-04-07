88ed07d7aecba1e327d3e38a40e1bbd3b506392c
priscadq <priscadq@gmail.com>
2018-04-07 11:49:56 -0300

    levantando archivo local con el server

---
+++ Clase1/servidor2740.js
@@ -0,0 +1,38 @@
+var http = require("http");
+
+var fs = require("fs")
+let text = "lalala"
+
+
+fs.readFile("archivo2.txt", function callback(err, data){
+
+    if(err) throw err
+    let text = data.toString();
+
+
+
+
+    
+var server = http.createServer(function(request, response) {
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write("<!DOCTYPE html>");
+    response.write("<html>");
+    response.write("<head>");
+    response.write("<title>Curso Backend</title>");
+    response.write("</head>");
+    response.write("<body>");
+    response.write( text  );
+    response.write("</body>");
+    response.write("</html>");
+    response.end();
+   });
+   
+   
+   
+   server.listen(80);
+   console.log("Server is listening");
+
+
+});
+
+
